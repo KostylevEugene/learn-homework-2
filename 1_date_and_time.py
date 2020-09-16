f391c92ea4787801de0d5a047c55d63b12d5e703
Eugene Kostylev <kostylev_evgenij@list.ru>
2020-09-16 21:05:59 +0500

    datetime and files tasks was completed

--- 1_date_and_time.py
+++ 1_date_and_time.py
@@ -7,21 +7,30 @@
 2. Превратите строку "01/01/20 12:10:03.234567" в объект datetime
 
 """
+from datetime import date, timedelta, datetime
+
+
 
 def print_days():
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    dt_today = date.today()
+    dt_yesterday = dt_today - timedelta(days=1)
+    dt_30days_ago = dt_today - timedelta(days=30)
 
+    print(f'Вчера было: {dt_yesterday}')
+    print(f'Сегодня: {dt_today}')
+    print(f'30 дней назад было: {dt_30days_ago}')
 
 def str_2_datetime(date_string):
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    date_dt = datetime.strptime(date_string, '%d/%m/%y %H:%M:%S.%f')
+    return date_dt
 
 if __name__ == "__main__":
     print_days()
--- 2_files.py
+++ 2_files.py
@@ -16,7 +16,33 @@ def main():
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    with open('referat.txt', 'r', encoding='utf-8') as newfile:
+        content = newfile.read()
+        
+    # подсчет длины строки
+    print(len(content))
+        
+    # деления строки на список
+    '''ch_content = content
+    ch_content = ch_content.replace('.', '')
+    ch_content = ch_content.replace(',', '')
+    ch_content = ch_content.replace(':', '')
+    ch_content = ch_content.replace('«', '')
+    ch_content = ch_content.replace('»', '')
+    ch_content = ch_content.replace('!', '')
+    ch_content = ch_content.replace('-', '')'''
+                
+    print(len(content.split(' ')))
+
+    # замена точки на восклицательные знаки
+    ch_content = content.replace('.', '!')
+    print(ch_content)
+
+    # запись результата в файл
+    with open('referat2.txt', 'w', encoding='utf-8') as newerfile:
+        newerfile.write(ch_content)
+
+
 
 if __name__ == "__main__":
     main()
